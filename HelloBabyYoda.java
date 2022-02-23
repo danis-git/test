9a40c4dacf372580d4be6a6fbfc54642a6ed53d7
Keppi <simon.kepplinger@gmail.com>
2022-02-23 12:35:40 +0100

    Update HelloBabyYoda.java

--- HelloBabyYoda.java
+++ HelloBabyYoda.java
@@ -1,7 +1,7 @@
 public class HelloBabyYoda  {
     
 	 public static void main(String[] args) {
-		System.out.printf("Hi my name is Baby Yoda\n ");
+		System.out.printf("Hi my name is Simili 😍\n ");
 	
 	}
 }
